a633e9cfedfcddcb799dcc3960fac63e93af1d72
DanielMendesDoAmaralLivance <daniel.amaral@livance.com.br>
2024-06-27 11:19:53 -0300

    Empty state, loadings e escondendo botões de ações de OS quando ela não estiver pendente

--- static/index.js
+++ static/index.js
@@ -31,44 +31,60 @@ const listOs = async () => {
   };
 
   $("#os-list").html(`
-          ${data.service_orders.map((service_order) => {
-    return `
-                      <div class="card my-2 fluid" style="min-width: 90%;">
-                          <div class="card-body">
-                          <div style="margin-bottom: 10px; display: flex; justify-content: space-between"> ${service_order.status === 1
-        ? `
-  <span class="badge badge-secondary">Pendente</span>`
-        : service_order.status === 2
-          ? `<span class="badge badge-success">Aprovada</span>`
-          : `<span class="badge badge-danger">Recusada</span>`
-      }
-                          </div>
+    <div class="spinner-border text-primary" role="status">
+      <span class="visually-hidden"></span>
+    </div>
+  `);
 
-                                  <h5 class="card-title">#${service_order.id
-        .toString()
-        .padStart(3, "0")} - ${service_order.title
-      }</h5>
-                              <hr/>
-                              <h6 class="card-subtitle mb-2 text-muted">
-                                  De ${formatDate(
-        service_order.start_date
-      )} à ${formatDate(service_order.end_date)}
-                              </h6>
-                              <p class="card-text">${service_order.description
-      }</p>
-                          </div>
-                          <div class="card-body">
-                              <a href="#" class="card-link" onclick="openOsModal(${service_order.id
-      })">Editar</a>
-                              <a href="#" class="card-link text-danger" onclick="openConfirmOrRefuseModal(${service_order.id
-      }, 'refuse-os')">Recusar</a>
-                              <a href="#" class="card-link text-success" onclick="openConfirmOrRefuseModal(${service_order.id
-      }, 'confirm-os')">Aprovar</a>
-                          </div>
-                      </div>
-                  `;
-  })}
+  setTimeout(() => {
+    if (!data?.service_orders || data?.service_orders.length === 0) {
+      $("#os-list").html(`
+        <p>
+          Nenhuma OS encontrada. Que tal criar clicando no botão flutuante abaixo?
+        </p>
       `);
+    } else {
+      $("#os-list").html(`
+      ${data.service_orders.map((service_order) => {
+        return `
+                  <div class="card my-2 fluid" style="min-width: 90%;">
+                      <div class="card-body">
+                      <div style="margin-bottom: 10px"> ${
+                        service_order.status === 1
+                          ? `
+<span class="badge badge-secondary">Pendente</span>`
+                          : service_order.status === 2
+                          ? `<span class="badge badge-success">Aprovada</span>`
+                          : `<span class="badge badge-danger">Recusada</span>`
+                      }
+                      </div>
+
+                              <h5 class="card-title">#${service_order.id
+                                .toString()
+                                .padStart(3, "0")} - ${service_order.title}</h5>
+                          <hr/>
+                          <h6 class="card-subtitle mb-2 text-muted">
+                              De ${formatDate(
+                                service_order.start_date
+                              )} à ${formatDate(service_order.end_date)}
+                          </h6>
+                          <p class="card-text">${service_order.description}</p>
+                      </div>
+                      ${
+                        service_order.status === 1
+                          ? `<div class="card-body">
+                          <a href="#" class="card-link" onclick="openOsModal(${service_order.id})">Editar</a>
+                          <a href="#" class="card-link text-danger" onclick="openConfirmOrRefuseModal(${service_order.id}, 'refuse-os')">Recusar</a>
+                          <a href="#" class="card-link text-success" onclick="openConfirmOrRefuseModal(${service_order.id}, 'confirm-os')">Aprovar</a>
+                      </div>`
+                          : ``
+                      }
+                  </div>
+              `;
+      })}
+  `);
+    }
+  }, 250);
 };
 
 window.onload = listOs;
